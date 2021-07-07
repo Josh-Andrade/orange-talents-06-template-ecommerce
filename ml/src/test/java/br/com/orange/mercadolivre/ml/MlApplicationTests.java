0b6a63e21991d0d308d02414a9c966b3fc398ca0
Joshua Andrade <45312862+Josh-Andrade@users.noreply.github.com>
2021-07-07 12:05:19 -0300

    criando-projeto
    projeto gerado e adicionado a pasta do repositorio

---
+++ ml/src/test/java/br/com/orange/mercadolivre/ml/MlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.orange.mercadolivre.ml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
